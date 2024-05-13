7d630a2b0a2fed28d36283c8d34314006ff20fa2
Jeanne Locker <jeanne@kaymbu.com>
2024-05-13 19:08:11 +0000

    added websockets + basic html

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+const token = 'cp0le61r01qnigekhh50cp0le61r01qnigekhh5g'
+const socket = new WebSocket(`wss://ws.finnhub.io?token=${token}`);
+
+// set up pg db
+// populate with stock symbols and info
+// take ss of it to include if I can't get a remote version working
+
+// frontend
+// grid to contain stock prices
+// useeffect based on orm?
+
+// (index) symbol | watched_at datetime
 
 function App() {
+  const watchlist = ['AAPL', 'NVDA']; // fetch results from PG; SELECT symbol FROM stonks, then shape to array of symbols
+  const [stockPrices, setStockPrices] = useState({});
+  useEffect(() => {
+
+    socket.addEventListener('open', function (event) {
+      watchlist.forEach(stock => {
+        socket.send(JSON.stringify({'type':'subscribe', 'symbol': stock}))
+      })
+    });
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+      const str = event.data;
+      const currSymbol = str.substring(
+        str.indexOf('"s"') + 4,
+        str.indexOf(',"t"')
+        ).replace(/"/g, '');
+      const currentStockPrice = str.substring(
+        str.indexOf(`"p"`) + 4,
+        str.indexOf(`,"s"`)
+      );
+      const stockTimestamp = str.substring(
+        str.indexOf(`"t"`) + 4,
+        str.indexOf(`,"v"`)
+      );
+      if (watchlist.includes(currSymbol)) {
+        const res = {};
+        res[currSymbol] = { price: currentStockPrice, timestamp: stockTimestamp };
+        setStockPrices({...stockPrices, ...res});
+      }
+    });
+
+    // return () => {
+    //     watchlist.forEach(stock => {
+    //         console.log('closing stock: ', stock);
+    //         socket.onopen = () => socket.send(JSON.stringify({ type: "unsubscribe", symbol: stock }));
+    //       })
+    //     }
+    }, [stockPrices, watchlist]);
+
+  const renderTableContents = (stocks) => {
+    const tableBody = [];
+    for (const stock in stocks) {
+      tableBody.push(
+        <tr>
+          <td>{stock}</td>
+          <td>{stockPrices[stock].price}</td>
+          <td>{stockPrices[stock].timestamp}</td>
+        </tr>
+      )
+    }
+    return tableBody;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table id='stonks'>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Price</th>
+            <th>Last Updated</th>
+          </tr>  
+        </thead>
+        <tbody>
+          {renderTableContents(stockPrices)}
+        </tbody>
+      </table>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const finnhub = require('finnhub');
+
+// CREATE TABLE stonks (symbol varchar(10), time_added timestamp);
+// INSERT INTO stonks VALUES ('AAPL', NOW());
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const watchlist = ['AAPL']; // fetch results from PG; SELECT symbol FROM stonks
+
+const finnhubClient = new finnhub.DefaultApi()
+
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+// app.get("/fetch-prices", (req, res) => {
+//   // console.log('req: ', req);
+//   // console.log('res: ', res);
+//   console.log('~~~~~~~~~~~~~~ ', streamStocks(watchlist));
+// });
+
+app.post("/add-symbol", (req, res) => {
+
 });
 
 app.listen(PORT, () => {
