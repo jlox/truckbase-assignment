07ec44f8a102ffd5f4ddeda8d5f8fa278b1a7094
Jeanne Locker <jeanne@kaymbu.com>
2024-05-15 19:00:57 +0000

    adding core files

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,99 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { toast, Toaster } from 'react-hot-toast';
 const token = 'cp0le61r01qnigekhh50cp0le61r01qnigekhh5g'
 const socket = new WebSocket(`wss://ws.finnhub.io?token=${token}`);
 
-// set up pg db
-// populate with stock symbols and info
-// take ss of it to include if I can't get a remote version working
-
-// frontend
-// grid to contain stock prices
-// useeffect based on orm?
-
-// (index) symbol | watched_at datetime
-
 function App() {
-  const watchlist = ['AAPL', 'NVDA']; // fetch results from PG; SELECT symbol FROM stonks, then shape to array of symbols
   const [stockPrices, setStockPrices] = useState({});
+  const [newStock, setNewStock] = useState('');
+  const [watchlist, setWatchlist] = useState([]);
+  
+  // initialize data and cleanup
   useEffect(() => {
-
-    socket.addEventListener('open', function (event) {
-      watchlist.forEach(stock => {
-        socket.send(JSON.stringify({'type':'subscribe', 'symbol': stock}))
+    // initialize watchlist with whats stored in SQL
+    axios
+      .get('http://localhost:3000/get-symbols')
+      .then((response) => {
+        setWatchlist(response.data);
+        socket.addEventListener('open', function (event) {
+          response.data.forEach(stock => {
+            socket.send(JSON.stringify({'type':'subscribe', 'symbol': stock}))
+          })
+        });
       })
-    });
-    // Listen for messages
-    socket.addEventListener('message', function (event) {
-      const str = event.data;
-      const currSymbol = str.substring(
-        str.indexOf('"s"') + 4,
-        str.indexOf(',"t"')
-        ).replace(/"/g, '');
-      const currentStockPrice = str.substring(
-        str.indexOf(`"p"`) + 4,
-        str.indexOf(`,"s"`)
-      );
-      const stockTimestamp = str.substring(
-        str.indexOf(`"t"`) + 4,
-        str.indexOf(`,"v"`)
-      );
-      if (watchlist.includes(currSymbol)) {
-        const res = {};
-        res[currSymbol] = { price: currentStockPrice, timestamp: stockTimestamp };
-        setStockPrices({...stockPrices, ...res});
-      }
-    });
+      .catch(err => {
+        console.error(err);
+      });
 
-    // return () => {
-    //     watchlist.forEach(stock => {
-    //         console.log('closing stock: ', stock);
-    //         socket.onopen = () => socket.send(JSON.stringify({ type: "unsubscribe", symbol: stock }));
-    //       })
-    //     }
-    }, [stockPrices, watchlist]);
+    // for cleanup
+    return () => {
+      watchlist.forEach(stock => {
+          console.log('closing subscription to: ', stock);
+          socket.onopen = () => {
+            socket.send(JSON.stringify({ type: "unsubscribe", symbol: stock }));
+            socket.close();
+          };
+      })
+    }
+  }, []);
+
+  // eventlistener for messages
+  useEffect(() => {
+    // listen to socket and parse data to display in table
+    const messageListener = (event) => {
+      const str = event.data;
+      const jsonMessage = JSON.parse(str);
+      const currSymbol = jsonMessage.data ? jsonMessage.data[0].s : null;
+      if (watchlist.length > 0) {
+        if (watchlist.includes(currSymbol)) {
+          const currentStockPrice = jsonMessage.data[0].p;
+          const stockTimestamp = jsonMessage.data[0].t;
+          const res = {};
+          res[currSymbol] = { price: currentStockPrice, timestamp: new Date(stockTimestamp).toString() };
+          setStockPrices((prevStockPrices) => { return { ...prevStockPrices, ...res } })
+        }
+      }
+    }
+    socket.onmessage = (e) => messageListener(e);
+  }, [watchlist]);
+
+  const handleChange = (e) => {
+    setNewStock(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // add new symbol to SQL and update local state
+    const addNewStock = (newStock) => {
+      axios
+        .post('http://localhost:3000/add-symbol', {params: newStock})
+        .then((response) => {
+          setWatchlist((prevWatchlist) => { return [...prevWatchlist, newStock]});
+          socket.send(JSON.stringify({'type':'subscribe', 'symbol': newStock}))
+          toast(`Successfully added ${newStock} to watchlist!`)
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+
+    if (watchlist.length > 0) {
+      // more validation would be put here (is this a real symbol, etc)
+      if (watchlist.includes(newStock)) {
+        toast(`${newStock} is already being watched!`)
+      }
+      else {
+        addNewStock(newStock);
+      }
+    }
+    else {
+      addNewStock(newStock)
+    }
+    
+  };
 
   const renderTableContents = (stocks) => {
     const tableBody = [];
@@ -59,8 +101,8 @@ function App() {
       tableBody.push(
         <tr>
           <td>{stock}</td>
-          <td>{stockPrices[stock].price}</td>
-          <td>{stockPrices[stock].timestamp}</td>
+          <td>{stocks[stock].price}</td>
+          <td>{stocks[stock].timestamp}</td>
         </tr>
       )
     }
@@ -69,6 +111,14 @@ function App() {
 
   return (
     <div className="App">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Toaster />
+        <label>Stock to add to watchlist: </label>
+        <input type="text" id="new-stock" name="new-stock" onChange={(e) => handleChange(e)}/>
+        <input type="submit"/>
+      </form>
+
+
       <table id='stonks'>
         <thead>
           <tr>
--- server/index.js
+++ server/index.js
@@ -1,29 +1,34 @@
 const express = require("express");
-const finnhub = require('finnhub');
+const cors = require('cors');
 
+// If I had set up PG, I would create a table with:
 // CREATE TABLE stonks (symbol varchar(10), time_added timestamp);
-// INSERT INTO stonks VALUES ('AAPL', NOW());
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-const watchlist = ['AAPL']; // fetch results from PG; SELECT symbol FROM stonks
+app.use(cors());
+app.use(express.json());
+const watchlist = [];
 
-const finnhubClient = new finnhub.DefaultApi()
-
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
+// because I used a websocket to fetch stock prices,
+// I don't have an endpoint here
 // app.get("/fetch-prices", (req, res) => {
 //   // console.log('req: ', req);
 //   // console.log('res: ', res);
-//   console.log('~~~~~~~~~~~~~~ ', streamStocks(watchlist));
 // });
 
-app.post("/add-symbol", (req, res) => {
+app.get("/get-symbols", (req, res) => {
+  // fetch results from PG; SELECT symbol FROM stonks
+  res.send(watchlist, 200);
+});
 
+app.post("/add-symbol", (req, res) => {
+  const symbol = req.body.params;
+  // Would add symbol validation here
+  // through ORM: INSERT INTO stonks VALUES (symbol, NOW());
+  watchlist.push(symbol);
+  res.send(200);
 });
 
 app.listen(PORT, () => {
